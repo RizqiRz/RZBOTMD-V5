a225fdb079b9d910cdb414db7644a93e5ba820da
Rizqi06 <compoterindo@gmail.com>
2022-10-22 17:28:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,11 +31,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6283865752867']
+global.ownernomer = "6283865752867"
+global.premium = ['6283865752867']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'RzBot-MD'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
@@ -62,13 +62,13 @@ global.limitawal = {
 }
 global.thumb = fs.readFileSync('./media/image/naze.jpg')
 global.faall = fs.readFileSync('./media/image/fake.jpg')
-global.mygit = 'https://github.com/nazedev'
-global.myyt = 'https://youtube.com/c/Nazedev'
-global.myytv = 'https://youtu.be/ey7qLuRBm5Y'
-global.mygc = "https://chat.whatsapp.com/Khpnk6svwIhCIvx91Xi0Vf"
-global.botname = 'Naze Dev'
-global.akulaku = 'Bot By Naze'
-global.ytname = 'YT NAZE'
+global.mygit = 'https://github.com/RizqiRz'
+global.myyt = '-'
+global.myytv = '-'
+global.mygc = "https://chat.whatsapp.com/HI3tPSjQa6BFF5CjsmpZh5"
+global.botname = 'RzBot-MD'
+global.akulaku = 'Bot By RZ06'
+global.ytname = 'RzBot-MD'
 
 
 let file = require.resolve(__filename)
